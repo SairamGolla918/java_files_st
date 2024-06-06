42c243fce5a13ccc4ceae45a350bd079bb2642b0
SairamGolla918 <gollasairam918@gmail.com>
2024-06-06 16:07:41 +0530

    saiee

---
+++ JavaPros/src/AbstractPack/Animal.java
@@ -0,0 +1,20 @@
+package AbstractPack;
+
+public abstract class Animal 
+{
+	int NoofLegs;
+	String AnimalType;
+	public Animal(int noofLegs, String animalType) {
+		NoofLegs = noofLegs;
+		AnimalType = animalType;
+	}
+	public void Walk(int NoofLegs)
+	{
+		if(NoofLegs >= 2)
+			System.out.println("Can Walk");
+		else
+			System.out.println("Can't Walk");
+	}
+	
+	
+}
